b25a7c2a224ec0ab83a6a626a5de39d835226cce
DylanLapham <69448342+DylanLapham@users.noreply.github.com>
2023-03-08 20:40:56 -0700

    Update F2C.asm

--- Set 1/F2C.asm
+++ Set 1/F2C.asm
@@ -3,6 +3,7 @@ Msg1: 	.asciiz		"What is the temperature in f? "
 Msg2: 	.asciiz		"The temperature in C is "
 
 .text
+	# load the prompt that asks for the temperature in fahrenheit:
 	li	$v0, 4
 	la	$a0, Msg1
 	syscall
@@ -12,16 +13,19 @@ Msg2: 	.asciiz		"The temperature in C is "
 	 
 	add	$t1, $v0, $zero
 	
+	# (F-32) * 5 / 9
 	addi 	$t1, $t1, -32
 	li	$t2, 5
 	li	$t3, 9
 	mul	$t1, $t1, $t2
 	div	$t1, $t1, $t3
 	
+	# load message 
 	li	$v0, 4
 	la	$a0, Msg2
 	syscall
 	
+	# display resulting celsius temperature.
 	li	$v0, 1
 	add	$a0, $zero, $t1
 	syscall
